ce3616c5c374f651c83373273df53a11c434dc24
ram <ram.tamang2058@gmail.com>
2023-09-16 07:32:20 +0545

    completed histrory of car of the parking owner

--- backend/Mypark/open_park/admin.py
+++ backend/Mypark/open_park/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from customer.models import Ticket
+from open_park.models import Parking
 # Register your models here.
+
+
+admin.site.register(Ticket)
+admin.site.register(Parking)
--- backend/Mypark/open_park/views.py
+++ backend/Mypark/open_park/views.py
@@ -23,9 +23,11 @@ def my_car_bookings(request):
     if request.user.is_authenticated and request.user.is_staff:
         # Get all parking codes owned by the logged-in staff member
         owner_parking_codes = Parking.objects.filter(owner=request.user).values_list('code', flat=True)
+        print(owner_parking_codes)
         
         # Find all tickets that match the parking_code in the owner's parking codes
-        parking_tickets = Ticket.objects.filter(parking_code__in=owner_parking_codes, vehicle_type__in='car')
+        parking_tickets = Ticket.objects.filter(parking_code__in=owner_parking_codes, vehicle_type='Car')
+        print(parking_tickets)
     else:
         return redirect('login')
     
